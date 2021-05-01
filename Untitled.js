70b963304d60b9aa5584cb64411e68acaf543356
limsungjong <82739304+limsungjong@users.noreply.github.com>
2021-05-01 16:02:15 +0900

    Delete Untitled.js

--- Untitled.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navbar__toggleButton'); 
-const  menu = document.querySelector('.navbar__menu');
-const social = document.querySelector('.navbar__social');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('active');
-    social.classList.toggle('active');
-})
